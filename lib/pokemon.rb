2870d6037f4b50187f982d5b86779b10d53fd7e4
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-08 16:34:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,13 +2,17 @@
 class Pokemon
   attr_accessor :name, :type, :db 
   attr_reader :id 
+<<<<<<< HEAD
   @@all = []
+=======
+>>>>>>> 80c7bed871ef3582f83a34269fb2114c4d8180f8
   
   def initialize(id:, name:, type:, db:)
     @id = id 
     @name = name 
     @type = type 
     @db = db 
+<<<<<<< HEAD
     @@all << self 
   end 
   
@@ -21,4 +25,7 @@ class Pokemon
     self.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], db: db)
   end 
   
+=======
+  end 
+>>>>>>> 80c7bed871ef3582f83a34269fb2114c4d8180f8
 end
